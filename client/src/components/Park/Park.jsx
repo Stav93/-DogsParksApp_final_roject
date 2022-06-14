e28050e59c835e57b8320575caae1e0ea3d6ca69
Stav <stavush93@gmail.com>
2022-06-14 20:37:21 +0300

    added likes state

--- client/src/components/Park/Park.jsx
+++ client/src/components/Park/Park.jsx
@@ -8,8 +8,10 @@ import {useUsersContext} from "../../Context/user-context"
 function Park({_id, name, city, street, users, OnUpdateParks, index}) {
   const usersCtx = useUsersContext();
   const [like, setLike] = useState(false)
+  const [likesCount, setLikesCount] = useState(0)
 
   useEffect(() => {
+    setLikesCount(users.length || 0)
     for (let i = 0; i < users.length; i++) {
       if(users[i]._id === usersCtx.user._id) {
         setLike(true);
@@ -23,11 +25,13 @@ function Park({_id, name, city, street, users, OnUpdateParks, index}) {
       usersCtx.onUserLike(_id)
       // users.push(usersCtx.user)
       OnUpdateParks(index, usersCtx.user, true)
+      setLikesCount((prev) => prev+1)
       setLike(true)
     } else {
       usersCtx.onUserUnlike(_id)
       // users = users.filter(user => user._id !==  usersCtx.user._id)
       OnUpdateParks(index, usersCtx.user, false)
+      setLikesCount((prev) => prev-1)
       setLike(false)
     }
   }
@@ -38,7 +42,7 @@ function Park({_id, name, city, street, users, OnUpdateParks, index}) {
         <label>{name}</label>
         <h3>City: {city}</h3>
         <h3>street: {street}</h3>
-        <button className={classes.like} onClick={likesHandler}>{like ? "unlike" : "Like"} ({ users ? users.length : 0 })</button>
+        <button className={classes.like} onClick={likesHandler}>{like ? "unlike" : "Like"} ({likesCount})</button>
         {/* <button className={classes.like} disabled={like} onClick={updatePark}>{like ? "unlike" : "Like"}</button> */}
       </Card>
     </div>
