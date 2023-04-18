224722aab63c3b3c81ed98cebcfdb31ee4b05c89
Stav <stavush93@gmail.com>
2023-04-18 18:29:43 +0300

    reset build proccess

--- server/server.mjs
+++ server/server.mjs
@@ -21,8 +21,8 @@ app.listen(port);
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(express.static("./build/index.html"))
-// app.use(express.static("../client/build"))
+// app.use(express.static("./build/index.html"))
+app.use(express.static("../client/build"))
 
 app.use('/api', AppRouter);
 
